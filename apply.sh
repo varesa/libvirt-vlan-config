d47fd8bc9d6f07fb6d91c4622d22b56b1849ed0e
Esa Varemo <esa@kuivanto.fi>
2019-08-10 22:29:46 +0300

    Allow storing of the config data in a different path

--- apply.sh
+++ apply.sh
@@ -1,7 +1,14 @@
 
 set -euo pipefail
 
-network=$1
+network=${1:-}
+
+([ -n "$network" ] && virsh net-list --name | head -n-1 | grep -q $network) || (echo "First argument must be name of libvirt network"; exit 1)
+
+path=${2:-}
+
+([ -n "$path" ] && [ -d $path/create ]) || (echo "Second argument must be a path that contains directories create/ and remove/"; exit 1)
+([ -n "$path" ] && [ -d $path/remove ]) || (echo "Second argument must be a path that contains directories create/ and remove/"; exit 1)
 
 current() {
     virsh net-dumpxml $network
@@ -29,7 +36,7 @@ get_name() {
    cat $1 | grep '<portgroup name' 
 }
 
-for portgroup_xml in create/*.xml; do
+for portgroup_xml in $path/create/*.xml; do
     if current | grep -q "$(get_name $portgroup_xml)"; then
         update $portgroup_xml
     else
@@ -37,7 +44,7 @@ for portgroup_xml in create/*.xml; do
     fi
 done
 
-for portgroup_xml in remove/*.xml; do
+for portgroup_xml in $path/remove/*.xml; do
     if current | grep -q "$(get_name $portgroup_xml)"; then
         remove $portgroup_xml
     else
